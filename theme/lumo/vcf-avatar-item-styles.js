da29a6c11ed6077d06188981585b7442d13e00e1
Nii Yeboah <nii@vaadin.com>
2019-10-17 17:25:16 +0300

    Fix styles issue

--- theme/lumo/vcf-avatar-item-styles.js
+++ theme/lumo/vcf-avatar-item-styles.js
@@ -3,13 +3,13 @@ import '@vaadin/vaadin-lumo-styles/sizing';
 import '@vaadin/vaadin-lumo-styles/typography';
 
 const theme = document.createElement('dom-module');
-  theme.id = 'vcf-avatar-item-lumo';
-    theme.setAttribute('theme-for', 'vcf-avatar-item');
-  theme.innerHTML = `
+theme.id = 'vcf-avatar-item-lumo';
+theme.setAttribute('theme-for', 'vcf-avatar-item');
+theme.innerHTML = `
     <template>
-      <style include="lumo-color lumo-sizing lumo-typography">
+      <style include="">
         :host {}
       </style>
     </template>
   `;
-  theme.register(theme.id);
+theme.register(theme.id);
